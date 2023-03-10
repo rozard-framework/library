be9472126425d960f40f117cae3920352b34b949
Al Muhdil Karim <al.muhdil.0@gmail.com>
2023-03-10 21:22:58 +0700

    version 5

--- config.php
+++ config.php
@@ -8,21 +8,6 @@
 
 
 
-/*** SITE */
-
-    if ( ! defined( 'MULTISITE' ) ) {
-        define( 'MULTISITE', true );
-    }
-
-    if ( ! MULTISITE === true  ) {
-        define( 'PATH_CURRENT_SITE', '/' );
-        define( 'SITE_ID_CURRENT_SITE', 1 );
-        define( 'BLOG_ID_CURRENT_SITE', 1 );
-        define( 'SUBDOMAIN_INSTALL', false );
-        define( 'DOMAIN_CURRENT_SITE', 'localhost' );
-    }
-
-
     /*** CORES */ 
 
 
